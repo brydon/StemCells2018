86d1288eb656acec2a72e1d3f4ab10c8c1b4134a
brydoneastman <brydoneastman@gmail.com>
2018-07-09 12:02:53 -0400

    Included Latex

--- python/plotting_code/plotgenerator.py
+++ python/plotting_code/plotgenerator.py
@@ -34,33 +34,32 @@ def std_plot(data, sd, show=True):
 if __name__ == "__main__":
     m, s = open_experiment("../../data/vary_eta1_eta2_r2/moran_r2_1.0")
 
-    """
-
     ls = np.linspace(0, 1, len(m))
-    print ls
 
     for i in range(6):
         plt.subplot(3, 2, i+1)
-        print ls[i*7]
-
         plt.xlabel(r"$\eta_2$")
         plt.tight_layout()
         plt.ylabel(r"$\rho_S(%0.1f, \eta_2)$" % ls[i*7])
         std_plot(m[i*7, :], s[i*7, :], show=False)
 
-    plt.show()
+    plt.savefig("../../images/constant_eta1_stackplot.png")
+    plt.clf()
 
     plt.xlabel(r"$\eta_2$")
     plt.ylabel(r"$\eta_1$")
     plt.title(r"Fixation Probability as a Function of Plasticity")
     contour_plot(m, show=False)
     plt.colorbar()
-    plt.show()
+    plt.savefig("../../images/contourplot.png")
+    plt.clf()
     
     plt.xlabel(r'$\eta_2$')
     plt.ylabel('Fixation Probability')
     plt.title('Average Fixation Probability for Wild-Type de-Differentiation')
-    std_plot(np.mean(m, 0), np.mean(s, 0))
+    std_plot(np.mean(m, 0), np.mean(s, 0), show=False)
+    plt.savefig("../../images/avg_eta1_plot.png")
+    plt.clf()
 
     for i, r2 in enumerate([0.25, 0.5, 0.75, 1.0]):
         plt.subplot(2, 2, i+1)
@@ -72,7 +71,8 @@ if __name__ == "__main__":
         plt.tight_layout()
         std_plot(np.mean(m, 0), np.mean(s, 0), show=False)
 
-    plt.show()
+    plt.savefig("../../images/avg_eta1_r2_stackplot1.png")
+    plt.clf()
 
     for i, r2 in enumerate([2.0, 3.0, 4.0, 5.0]):
         plt.subplot(2, 2, i+1)
@@ -84,7 +84,8 @@ if __name__ == "__main__":
         plt.tight_layout()
         std_plot(np.mean(m, 0), np.mean(s, 0), show=False)
 
-    plt.show()
+    plt.savefig("../../images/avg_eta1_r2_stackplot2.png")
+    plt.clf()
 
     for i, u2 in enumerate([0.05, 0.1, 0.15, 0.2]):
         plt.subplot(2, 2, i + 1)
@@ -96,7 +97,8 @@ if __name__ == "__main__":
         plt.tight_layout()
         std_plot(np.mean(m, 0), np.mean(s, 0), show=False)
 
-    plt.show()
+    plt.savefig("../../images/avg_eta1_u2_stackplot1.png")
+    plt.clf()
 
     for i, u2 in enumerate([.3, 0.4, 0.5, 0.6]):
         plt.subplot(2, 2, i + 1)
@@ -108,7 +110,8 @@ if __name__ == "__main__":
         plt.tight_layout()
         std_plot(np.mean(m, 0), np.mean(s, 0), show=False)
 
-    plt.show()
+    plt.savefig("../../images/avg_eta1_u2_stackplot2.png")
+    plt.clf()
 
     for i, u2 in enumerate([0.7, 0.8, 0.9, 0.95]):
         plt.subplot(2, 2, i + 1)
@@ -120,17 +123,5 @@ if __name__ == "__main__":
         plt.tight_layout()
         std_plot(np.mean(m, 0), np.mean(s, 0), show=False)
 
-    plt.show()
-
-    """
-
-    m, s = open_experiment("../../data/vary_eta1_eta2_r2/moran_r2_1.0")
-    for i, r in enumerate([0, 12, 23, 35]):
-        plt.subplot(2, 2, i + 1)
-
-        plt.xlabel(r'$\eta_2$')
-        plt.ylabel(r'Fix. Prob.')
-        plt.tight_layout()
-        std_plot(m[r,:], s[r,:], show=False)
-
-    plt.show()
+    plt.savefig("../../images/avg_eta1_u2_stackplot3.png")
+    plt.clf()
